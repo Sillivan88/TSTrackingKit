1f9a818ec9d49e53898b22425e8cff7d2daa4bad
Thomas Sillmann <thomas@thomassillmann.de>
2015-10-01 11:03:37 +0200

    Added new target to create an universal framework.

---
+++ Output/TSTrackingKit-Debug-iphoneuniversal/TSTrackingKit.framework/Headers/TSTrackingKit.h
@@ -0,0 +1,19 @@
+//
+//  TSTrackingKit.h
+//  TSTrackingKit
+//
+//  Created by Thomas Sillmann on 28.09.15.
+//  Copyright © 2015 Thomas Sillmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TSTrackingKit.
+FOUNDATION_EXPORT double TSTrackingKitVersionNumber;
+
+//! Project version string for TSTrackingKit.
+FOUNDATION_EXPORT const unsigned char TSTrackingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TSTrackingKit/PublicHeader.h>
+#import <TSTrackingKit/TSTracking.h>
+#import <TSTrackingKit/TSEvent.h>
